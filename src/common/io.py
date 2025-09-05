ec7ae0ba25d2fbc65946d05ba216d552ce22647b
FirstFlush <firstflush@protonmail.com>
2025-09-04 17:53:26 -0700

    feat: create text-data reader/writers

--- src/common/io.py
+++ src/common/io.py
@@ -24,6 +24,18 @@ class ConsoleWriter(BaseIOHandler):
 
 class FileReader(BaseIOHandler):
 
+    def read_text(self, file_path: Path) -> str:
+        with open(file_path, "r", encoding="utf-8") as f:
+            data = f.read()
+        logger.debug(f"Loaded text file from {file_path}")
+        return data
+    
+    def read_text_lines(self, file_path: Path) -> list[str]:
+        with open(file_path, "r", encoding="utf-8") as f:
+            data = f.readlines()
+        logger.debug(f"Loaded text file from {file_path}")
+        return data
+
     def json_from_file(self, file_path: Path) -> Any:
         with open(file_path, "r", encoding="utf-8") as f:
             data = json.load(f)
@@ -53,6 +65,10 @@ class FileWriter(BaseIOHandler):
             file_name = f"{file_name}.{ext}"
         return self.output_dir / file_name
 
+    def save(self, output_path: Path, text: str):
+        with open(output_path, "w", encoding="utf-8") as out:
+            out.write(text)
+
     def save_json(self, output_path: Path, json_data: Any):
         with open(output_path, "w", encoding="utf-8") as out:
             json.dump(json_data, out, indent=2)
@@ -61,34 +77,3 @@ class FileWriter(BaseIOHandler):
         shutil.copy2(src, dst, follow_symlinks=follw_symlinks)
         logger.info(f"Copied {src.name} → {dst.name}")
 
-
-# class FileManager:
-
-#     def __init__(self, output_dir: Path):
-#         self.output_dir = output_dir
-#         self.output_dir.mkdir(parents=True, exist_ok=True)
-
-#     def output_path(self, input_path: Path, ext: str) -> Path:
-#         ext = ext.lstrip(".")
-#         return self.output_dir / f"{input_path.stem}__{self._timestamp()}.{ext}"
-
-#     def json_from_file(self, file_path: Path) -> Any:
-#         with open(file_path, "r", encoding="utf-8") as f:
-#             data = json.load(f)
-#         logger.debug(f"Loaded JSON data from {file_path}")
-#         return data
-    
-#     def json_from_files(self, dir_path: Path, **globbing_kwargs) -> Any:
-#         file_paths = dir_path.glob("*.json", **globbing_kwargs)
-#         data_aggregate = []
-#         for file_path in file_paths:
-#             data = self.json_from_file(file_path)
-#             if isinstance(data, Iterable):
-#                 data_aggregate.extend(data)
-
-#     def save_json(self, output_path: Path, json_data: Any):
-#         with open(output_path, "w", encoding="utf-8") as out:
-#             json.dump(json_data, out, indent=2)
-
-#     def _timestamp(self) -> str:
-#         return datetime.now(tz=timezone.utc).strftime("%y-%m-%d_%H-%M-%S")
